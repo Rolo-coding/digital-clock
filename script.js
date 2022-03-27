9ae676b2ce9665c1b292ef19ba9e8b8799bbf69d
Hotaro <kay.sothearo2019@rupp.edu.kh>
2022-03-27 08:22:05 +0700

    Check am or pm and adding 0 to value < 10
    Noiceeee!

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ setInterval(() => {
   let min = date.getMinutes();
   let sec = date.getSeconds();
 
+  let d = hour > 12 ? "PM" : "AM";
+  hour = hour > 12 ? hour - 12 : hour;
+  hour = hour == 0 ? (hour = 12) : hour;
+
+  // adding 0 to all value that less than 10
+  sec = sec < 10 ? "0" + sec : sec;
+  min = min < 10 ? "0" + min : min;
+  hour = hour < 10 ? "0" + hour : hour;
+
   document.querySelector(".hour-num").innerText = hour;
   document.querySelector(".min-num").innerText = min;
   document.querySelector(".sec-num").innerText = sec;
+  document.querySelector(".am-pm").innerText = d;
 }, 1000);
